a664f429314ffe07cb41f47f55917e87408a8de6
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-07 10:38:31 +0100

    done

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,9 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: "/auth/google/callback",
+            callbackURL:
+                "https://testing-mauve-five.vercel.app/auth/google/callback",
+            proxy: true,
         },
         async (accessToken, refreshToken, profile, done) => {
             const existingUser = await User.findOne({ googleId: profile.id });
