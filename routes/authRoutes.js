fe583ec91133117cde8dab0ab006ab37b4f4c0e3
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-07 13:21:19 +0100

    new

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,10 +13,13 @@ module.exports = (app) => {
 
     app.get("/auth/google/callback", passport.authenticate("google"));
 
-    app.get("/api/logout", (req, res) => {
-        req.session = null;
+    app.post("/api/logout", (req, res) => {
+        
+        
         req.logout();
         res.send(req.user);
+       
+         
     });
     app.get("/api/current_user", (req, res) => {
         res.send(req.user);
