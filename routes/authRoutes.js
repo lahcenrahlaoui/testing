0366625e6d58bb1b0aea01b3f4d504a6eb6974e8
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-08 16:20:50 +0100

    new user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -19,6 +19,6 @@ module.exports = (app) => {
     });
 
     app.get("/api/current_user", (req, res) => {
-        res.send({ wx: req["user"], x: "s" });
+        res.send({ wx: req["user"], x: "se" });
     });
 };
