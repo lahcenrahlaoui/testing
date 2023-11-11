fd11922c014ca6f37e59a0c61b5dd9914e0ceca2
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-11 22:47:01 +0100

    new

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 
-import session from "express-session";
+import session from 'express-session';
 
 const passport = require("passport");
 const cors = require("cors");
@@ -13,41 +13,20 @@ require("./services/passport");
 mongoose.connect(keys.database);
 const app = express();
 
-var allowedDomains = [
-    "https://testing-client-ashen.vercel.app",
-    "https://localhost:3000",
-    "http://localhost:3000",
-];
-// app.use(
-//     cors({
-//         origin: function (origin, callback) {
-//             // bypass the requests with no origin (like curl requests, mobile apps, etc )
-//             if (!origin) return callback(null, true);
-
-//             if (allowedDomains.indexOf(origin) === -1) {
-//                 var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-//                 return callback(new Error(msg), false);
-//             }
-//             return callback(null, true);
-//         },
-//         credentials: true,
-//     })
-// );
-app.use(cors())
+app.use(cors({ origin: "https://localhost:3000", credentials: true }))
 app.set("trust proxy", 1);
 
 app.use(
-    session({
-        secret: "secretcode",
-        resave: true,
-        saveUninitialized: true,
-        cookie: {
-            sameSite: "none",
-            secure: true,
-            maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
-        },
-    })
-);
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: "none",
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7 // One Week
+    }
+  }))
 // app.use(
 //     cookieSession({
 //         maxAge: 30 * 24 * 60 * 60 * 1000,
