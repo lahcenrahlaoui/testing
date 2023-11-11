cfa657af835a00767abab5ac5c4f4ab0e9be9a8d
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-11 22:40:02 +0100

    new

--- index.js
+++ index.js
@@ -18,21 +18,22 @@ var allowedDomains = [
     "https://localhost:3000",
     "http://localhost:3000",
 ];
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            // bypass the requests with no origin (like curl requests, mobile apps, etc )
-            if (!origin) return callback(null, true);
+// app.use(
+//     cors({
+//         origin: function (origin, callback) {
+//             // bypass the requests with no origin (like curl requests, mobile apps, etc )
+//             if (!origin) return callback(null, true);
 
-            if (allowedDomains.indexOf(origin) === -1) {
-                var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        },
-        credentials: true,
-    })
-);
+//             if (allowedDomains.indexOf(origin) === -1) {
+//                 var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+//                 return callback(new Error(msg), false);
+//             }
+//             return callback(null, true);
+//         },
+//         credentials: true,
+//     })
+// );
+app.use(cors())
 app.set("trust proxy", 1);
 
 app.use(
