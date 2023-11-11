6dc50991e129bcf923af43bcb446361b19f87ce5
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-11 15:38:34 +0100

    server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 
-const cookieSession = require("cookie-session");
+import session from 'express-session';
 
 const passport = require("passport");
 const cors = require("cors");
@@ -12,8 +12,20 @@ require("./services/passport");
 
 mongoose.connect(keys.database);
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "https://testing-client-ashen.vercel.app", credentials: true }))
+app.set("trust proxy", 1);
 
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: "none",
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7 // One Week
+    }
+  }))
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
