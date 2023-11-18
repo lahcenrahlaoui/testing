661c23377f4847e8823d5971aa4d6f88d10d21cd
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-18 15:21:54 +0100

    delete dispatch

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,10 +1,8 @@
-module.exports = (req , res , next ) =>{
-
-    if(!req.user){
-        return res.send("you must login")
+module.exports = (req, res, next) => {
+    console.log(req.user?.name || "there is no user ")
+    if (!req.user) {
+        return res.send("you must login");
     }
 
-
-    next()
-
-}
+    next();
+};
