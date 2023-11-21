eb963b004a51c9fd22ad970889ae8f4fea3c0cbd
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-21 18:15:12 +0100

    add console

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,4 +1,5 @@
 module.exports = (req, res, next) => {
+    console.log("user >>>>>>>>>>>  " + req)
     console.log("user >>>>>>>>>>>  " + req.user)
     if (!req.user) {
         return res.send("you must login");
