71c2327da302cf8dcb73324c717e01fcfad7e91c
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-21 18:23:53 +0100

    object object

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
-    console.log("user >>>>>>>>>>>  " + req)
-    console.log("user >>>>>>>>>>>  " + req.user)
+    console.log("user >>>>>>>>>>>  ");
+    console.log(req);
+    console.log("user >>>>>>>>>>>  " + req.user);
     if (!req.user) {
         return res.send("you must login");
     }
