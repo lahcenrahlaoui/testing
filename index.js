0c1b7edb752550e6c4438910a460fcc267603f65
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-21 15:17:16 +0100

    get old version

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
 // packages
 const express = require("express");
 const mongoose = require("mongoose");
-
 const cors = require("cors");
 const passport = require("passport");
-
 const session = require("express-session");
 const cookieSession = require("cookie-session");
 const cookieParser = require("cookie-parser");
-
-const bodyParser = require("body-parser");
-
 // keys
 const keys = require("./config/keys");
 const PORT = process.env.PORT || 5000;
 const ENVIREMENT = process.env.ENVIREMENT || "development";
-
 // files
 const postsRoute = require("./routes/postsRoute");
 require("./models/User");
 require("./services/passport");
-
 const app = express();
-
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
-
-// app.use(bodyParser.json({ limit: "30mb" }));
+app.use((_req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    console.log("xxxxxxxxxxxxxxxx");
+    console.log("xxxxxxxxxxxxxxxx");
+    next();
+});
 
 // passport package
-
 if (ENVIREMENT === "development") {
     app.use(cors());
-
     app.use(
         cookieSession({
             maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -52,12 +47,16 @@ if (ENVIREMENT === "development") {
         session({
             sameSite: "none",
             secret: keys.cookieKey,
+            key: "userid",
             resave: false,
             saveUninitialized: false,
             cookie: {
                 sameSite: "none",
-                // maxAge: 30 * 60 * 60 * 24 * 1000, // One Week
+                secure: true,
                 maxAge: 30 * 60 * 60 * 24 * 1000, // One Week
+                // maxAge: 30 * 60 * 60 * 24 * 1000, // One Week
+                expires: 30 * 60 * 60 * 24 * 1000, // One Week
+                httpOnly: false,
             },
             httpOnly: false,
         })
@@ -66,11 +65,9 @@ if (ENVIREMENT === "development") {
 // passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
-
 // routes
 require("./routes/authRoutes")(app);
 app.use("/api", postsRoute);
-
 // connect to database
 mongoose.connect(keys.database).then(() => {
     app.listen(PORT);
