7428f70e4475a1555452230176ec96a76d4eb15d
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2023-11-23 21:04:27 +0100

    isAuthenticated

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
     console.log("req.user++++++++++++++++++++++++");
     console.log("req.user++++++++++");
-    console.log(req);
+    console.log(req.isAuthenticated());
     console.log("req.user++++++++");
     console.log(req.user);
 
